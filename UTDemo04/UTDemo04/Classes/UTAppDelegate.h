3ec7fc15d69a1dfccb9a08559f6184909590e890
Hivan Du <ivandoo75@gmail.com>
2013-06-02 00:48:05 +0800

    The directory structure

---
+++ UTDemo04/UTDemo04/Classes/UTAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  UTAppDelegate.h
+//  UTDemo04
+//
+//  Created by Hivan Du on 13-6-2.
+//  Copyright (c) 2013年 Hivan Du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UTDragonView.h"
+
+@interface UTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
